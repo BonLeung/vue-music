8d84a5a814a1149f31d0babeff8bdcf9f129174c
BonLeung <654592397@qq.com>
2017-07-24 00:45:16 +0800

    commit

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -16,7 +16,7 @@
             <ul>
               <li v-for="item in discList" class="item">
                 <div class="icon">
-                  <img width="60" height="60" :src="item.imgurl" alt="">
+                  <img width="60" height="60" v-lazy="item.imgurl" alt="">
                 </div>
                 <div class="text">
                   <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,6 +26,9 @@
             </ul>
           </div>
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
   </scroll>
   </div>
 </template>
@@ -33,6 +36,7 @@
 <script>
 import Slider from 'base/slider/slider'
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
@@ -71,7 +75,8 @@ export default {
   },
   components: {
     Slider,
-    Scroll
+    Scroll,
+    Loading
   }
 }
 </script>
@@ -120,5 +125,10 @@ export default {
             color: $color-text;
           .desc
             color: $color-text-d
+    .loading-container
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      transform: translateY(-50%);
 
 </style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,93 @@
 <template lang="html">
-  <div class="singer"></div>
+  <div class="singer">
+    <list-view :data="singers"></list-view>
+  </div>
 </template>
 
 <script>
+import ListView from 'base/list-view/list-view'
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
 export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(response => {
+        if (response.code === ERR_OK) {
+          this.singers = this._normalizeSinger(response.data.list)
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(
+            new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            })
+          )
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(
+          new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          })
+        )
+      })
+
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let k in map) {
+        let val = map[k]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  },
+  components: {
+    ListView
+  }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+.singer
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
 </style>
