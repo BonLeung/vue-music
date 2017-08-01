379dc977bd99f3ab1a712a2f8f6a8404eada5ee9
BonLeung <654592397@qq.com>
2017-08-01 18:32:45 +0800

    commit

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -5,7 +5,49 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/song'
+
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'singer'
+    ])
+  },
+  created() {
+    this._getDetail()
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then(response => {
+        if (response.code === ERR_OK) {
+          this.songs = this._normalizeSongs(response.data.list)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  }
 }
 </script>
 
