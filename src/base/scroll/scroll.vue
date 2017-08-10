06285ebc66020b7841e56161772dd3fbc4112a34
BonLeung <654592397@qq.com>
2017-08-10 09:48:15 +0800

    sommit

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -24,6 +24,10 @@ export default {
     listenScroll: {
       type: Boolean,
       default: false
+    },
+    pullup: {
+      type: Boolean,
+      default: false
     }
   },
   mounted() {
@@ -45,6 +49,14 @@ export default {
         let that = this
         this.scroll.on('scroll', pos => {
           that.$emit('scroll', pos)
+        })
+      }
+
+      if (this.pullup) {
+        this.scroll.on('scrollEnd', () => {
+          if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+            this.$emit('scrollToEnd')
+          }
         })
       }
     },
--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <scroll class="suggest" :data="result">
+  <scroll class="suggest" :data="result" :pullup="pullup" @scrollToEnd="searchMore" ref="suggest">
     <ul class="suggest-list">
       <li class="suggest-item" v-for="item in result">
         <div class="icon">
@@ -9,17 +9,20 @@
           <p class="text" v-html="getDisplayName(item)"></p>
         </div>
       </li>
+      <loading v-show="hasMore"></loading>
     </ul>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 import { search } from 'api/search'
 import { ERR_OK } from 'api/config'
 import { createSong } from 'common/js/song'
 
 const TYPE_SINGER = 'singer'
+const PER_PAGE = 20
 
 export default {
   props: {
@@ -35,7 +38,9 @@ export default {
   data() {
     return {
       page: 1,
-      result: []
+      result: [],
+      pullup: true,
+      hasMore: true
     }
   },
   methods: {
@@ -54,11 +59,33 @@ export default {
       }
     },
     search() {
-      search(this.query, this.page, this.showSinger).then((res) => {
+      this.hasMore = true
+      this.page = 1
+      this.$refs.suggest.scrollTo(0, 0)
+      search(this.query, this.page, this.showSinger, PER_PAGE).then((res) => {
         if (res.code === ERR_OK) {
           this.result = this._genResult(res.data)
+          this._checkMore(res.data)
         }
       })
+    },
+    searchMore() {
+      if (!this.hasMore) {
+        return
+      }
+      this.page++
+      search(this.query, this.page, this.showSinger, PER_PAGE).then((res) => {
+        if (res.code === ERR_OK) {
+          this.result = this.result.concat(this._genResult(res.data))
+          this._checkMore(res.data)
+        }
+      })
+    },
+    _checkMore(data) {
+      const song = data.song
+      if (!song.list.length || (song.curnum + song.curpage * PER_PAGE) > song.totalnum) {
+        this.hasMore = false
+      }
     },
     _normalizeSongs(list) {
       let ret = []
@@ -86,7 +113,8 @@ export default {
     }
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   }
 }
 </script>
